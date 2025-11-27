ceea942eba4061a0898992329b661b21f542e22a
chandra <chandrajayaadriadi@gmail.com>
2025-11-27 22:26:13 +0700

    first commit

---
+++ src/Mergesort.java
@@ -0,0 +1,66 @@
+public class Mergesort {
+    
+    public static void merge(int[] arr, int left, int mid, int right) {
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+
+        
+        for (int i = 0; i < n1; ++i)
+            L[i] = arr[left + i];
+        for (int j = 0; j < n2; ++j)
+            R[j] = arr[mid + 1 + j];
+
+        int i = 0, j = 0;
+        int k = left;
+
+        while (i < n1 && j < n2) {
+            if (L[i] >= R[j]) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+
+   
+    public static void mergeSort(int[] arr, int left, int right) {
+        if (left < right) {
+            int mid = (left + right) / 2;
+            mergeSort(arr, left, mid);
+            mergeSort(arr, mid + 1, right);
+            merge(arr, left, mid, right);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] data = {45, 12, 78, 23, 56, 89, 34, 67};
+
+        System.out.println("Data sebelum diurutkan:");
+        for (int num : data)
+            System.out.print(num + " ");
+
+        mergeSort(data, 0, data.length - 1);
+
+        System.out.println("\n\nData setelah diurutkan (terbesar ke terkecil):");
+        for (int num : data)
+            System.out.print(num + " ");
+    }
+}
